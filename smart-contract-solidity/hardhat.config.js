f6404061658e564fe06b50ae565c2d9ae47ffbd3
Farooq <farooqdaadkhan@gmail.com>
2022-12-02 00:07:07 +0500

    smallchanges

--- smart-contract-solidity/hardhat.config.js
+++ smart-contract-solidity/hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: '0.8.17',
   networks: {
     goerli: {
-      url: 'https://omniscient-wandering-hexagon.ethereum-goerli.discover.quiknode.pro/5cf0097488240ec6c843cc7c287fba03c7dabdf5/',
-      accounts: ['491619110b7cdb4c31bc6baeac04d7b0eb987e28f9555ef7801df6a61ac02b89'],
+      url: process.env.QUICKNODE_API_URL,
+      accounts: [process.env.PVT_KEY],
     },
   },
 };
